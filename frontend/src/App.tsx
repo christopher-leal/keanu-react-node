133af78e80d76ce434a86f78bcef7202ecae7f87
Christopher Leal <christopher.gomez1@nordstrom.com>
2024-08-07 15:15:25 -0600

    frontend initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const handleClick=()=>{
+    fetch('http://localhost:3000/api').then(res=>res.json()).then(res=>{
+      console.log(res)
+    })
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick}>
+          make request
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
