1de2cc7eab807ec417267e48e7d8ab9f87725934
Christopher Leal <christopher.gomez1@nordstrom.com>
2024-08-08 16:59:57 -0600

    envs updated

--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -7,6 +7,4 @@ export const envs = {
   MONGO_HOST: get("MONGO_HOST").required().asString(),
   MONGO_PORT: get("MONGO_PORT").required().asString(),
   MONGO_DATABASE: get("MONGO_DATABASE").required().asString(),
-  MONGO_USERNAME: get("MONGO_USERNAME").required().asString(),
-  MONGO_PASSWORD: get("MONGO_PASSWORD").required().asString(),
 };
