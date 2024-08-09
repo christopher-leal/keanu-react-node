de2799bff03b73b73466eb5425138710ce8abb06
Christopher Leal <christopher.gomez1@nordstrom.com>
2024-08-08 19:21:45 -0600

    files moved

--- frontend/src/graphql/queries.ts
+++ frontend/src/graphql/queries.ts
@@ -13,19 +13,34 @@ export const apolloClient = new ApolloClient({
 });
 
 export const getImage = gql`
-  query (
-    $width: String!
-    $height: String
-    $greyScale: Boolean
+  query KeanuImage(
+    $width: Int!
+    $height: Int
+    $greyscale: Boolean
     $young: Boolean
+    $historyId: String
   ) {
     keanuImage(
       width: $width
       height: $height
-      greyScale: $greyScale
+      greyscale: $greyscale
       young: $young
+      historyId: $historyId
     ) {
       url
+    }
+  }
+`;
+
+export const getHistory = gql`
+  query GetHistory($limit: Int) {
+    getHistory(limit: $limit) {
+      url
+      greyscale
+      height
+      width
+      young
+      id
     }
   }
 `;
