f5c71bfaddc6164d4cb5448561eb2671722a600c
Christopher Leal <christopher.gomez1@nordstrom.com>
2024-08-08 19:19:39 -0600

    format applied

--- backend/src/graphql/dataSources/keanu-api.ts
+++ backend/src/graphql/dataSources/keanu-api.ts
@@ -1,28 +1,28 @@
-import { RESTDataSource } from '@apollo/datasource-rest';
-import { ImageDto } from '../../dtos/image.dto';
+import { RESTDataSource } from "@apollo/datasource-rest";
+import { ImageDto } from "../../dtos/image.dto";
 
 type Image = {
-  url: string
-}
+  url: string;
+};
 export class KeanuAPI extends RESTDataSource {
-  override baseURL = 'https://placekeanu.com/';
+  override baseURL = "https://placekeanu.com/";
 
   async getImage(imageDto: ImageDto): Promise<Image> {
-    const {width, height, greyscale, young} = imageDto
-    if(!width){
-      throw new Error('Width field is a mandatory field')
+    const { width, height, greyscale, young } = imageDto;
+
+    if (!width) {
+      throw new Error("Width field is a mandatory field");
     }
-    let url = `${width}`
-    if(height){
-      url = `${url}/${height}`
+    let url = `${width}`;
+    if (height) {
+      url = `${url}/${height}`;
     }
-    if(greyscale||young){
-      url = `${url}/${greyscale?'g':''}${young?'y':''}`
+    if (greyscale || young) {
+      url = `${url}/${greyscale ? "g" : ""}${young ? "y" : ""}`;
     }
     const data = await this.get(url);
     return {
-      url: data
+      url: data,
     };
   }
- 
 }
